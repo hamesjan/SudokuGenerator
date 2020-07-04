018522de74fb18c9856329c8a9ad5e02aa461bc3
James Jongyoon Han <jameshan@Jamess-MacBook-Pro.local>
2020-07-03 20:18:56 -0700

    adding some more rules

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -1,5 +1,5 @@
-from random import randrange
 import numpy as np
+import random
 
 def generate():
     """
@@ -20,24 +20,71 @@ def generate():
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0]
     ]
-    for row in range(10):
-        for column in range(10):
+    for row in range(9):
+        for column in range(9):
+            print(np.asmatrix(board))
+            print(row, column)
+            square = get_square(board, row, column)
+            print(square)
             while True:
-                val = randrange(10)
-                if checkRow(row, val) and checkColumn(column, val) and checkSquare(square, val):
+                val = random.randint(1, 9)
+                print(val)
+                if check_row(board, row, val) and check_column(board, column, val) and check_square(square, val):
                     board[row][column] = val
+                    print('success')
                     break
-                else: continue
+
+
     print(np.asmatrix(board))
 
 
+def get_square(board, row, column):
+    if row < 3:
+        if column < 3:
+            square = [board[i][0:3] for i in range(0, 3)]
+        elif column < 6:
+            square = [board[i][3:6] for i in range(0, 3)]
+        else:
+            square = [board[i][6:9] for i in range(0, 3)]
+    elif row < 6:
+        if column < 3:
+            square = [board[i][0:3] for i in range(3, 6)]
+        elif column < 6:
+            square = [board[i][3:6] for i in range(3, 6)]
+        else:
+            square = [board[i][6:9] for i in range(3, 6)]
+    else:
+        if column < 3:
+            square = [board[i][0:3] for i in range(6, 9)]
+        elif column < 6:
+            square = [board[i][3:6] for i in range(6, 9)]
+        else:
+            square = [board[i][6:9] for i in range(6, 9)]
+    return square
 
-def checkRow(row, val):
+
+def check_row(board, row, val):
+    if val not in board[row]:
+        return True
+    return False
 
 
-def checkColumn(column, val):
+def check_column(board, column, val):
+    temp = []
+    for each in board:
+        temp.append(each[column])
+    if val not in temp:
+        return True
+    return False
 
-def checkSquare(square, val):
+
+def check_square(square, val):
+    for each in square:
+        if val not in each:
+            continue
+        else:
+            return False
+    return True
 
 
 def main():
